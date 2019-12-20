bddb3b97578e794c0ef16db0d6b2dbd48712ad71
sapphi-red <green@sapphi.red>
2019-12-20 20:51:22 +0900

    Update

---
+++ precache-manifest.2fa7d826d9d1b107e7f0120568fca0ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21f22d2dbe736671877cebf2f7e6e59",
+    "url": "/206b5e87bc011813b4b7.worker.js"
+  },
+  {
+    "revision": "d535950438739d54c887503ba0c5991c",
+    "url": "/206b5e87bc011813b4b7.worker.js.LICENSE"
+  },
+  {
+    "revision": "da1909900730e8a2107856e98cf47b37",
+    "url": "/323dbd74d82b749b483a.worker.js"
+  },
+  {
+    "revision": "d535950438739d54c887503ba0c5991c",
+    "url": "/323dbd74d82b749b483a.worker.js.LICENSE"
+  },
+  {
+    "revision": "f87fc56dd268593779eb085ee5e17af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97905fb2881832bfc079",
+    "url": "/static/css/main.8a78db86.chunk.css"
+  },
+  {
+    "revision": "ab69549418ea0888a691",
+    "url": "/static/js/2.d36b8fe8.chunk.js"
+  },
+  {
+    "revision": "f703dab3c5ad88e5a850b0fe8e60fa96",
+    "url": "/static/js/2.d36b8fe8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97905fb2881832bfc079",
+    "url": "/static/js/main.d5788198.chunk.js"
+  },
+  {
+    "revision": "7157fba174487521f253",
+    "url": "/static/js/runtime-main.ea472412.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1be9a8243718fdf6f02fa3bc282cf0ba.js"
+  "/precache-manifest.2fa7d826d9d1b107e7f0120568fca0ca.js"
 );
 
 self.addEventListener('message', (event) => {
